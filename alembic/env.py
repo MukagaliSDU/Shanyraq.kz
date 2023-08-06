215fdee7a2ed632e709937a3d38f6ee794485786
Mukagali <mukgaly.omirzak2003@gmail.com>
2023-08-06 12:59:59 +0600

    alembic part is commit

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from app.database import Base
 from app.repositories.users_repository import User
 from app.repositories.announcement_repository import Announcement
 from app.repositories.comments_repository import Comment
+from app.repositories.favorites_repository import Favorites
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
